b5afbaec4256ec4266c23930dedad04e9f719cce
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2020-03-22 21:15:07 +0300

    Updates

---
+++ precache-manifest.64384c4f005a4cd29f65234ee4438cc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f9ce92535bbc4e142799b9dedb74e5",
+    "url": "/maroom/index.html"
+  },
+  {
+    "revision": "1fb1b240949ac3bbb29f",
+    "url": "/maroom/static/css/2.72d5056d.chunk.css"
+  },
+  {
+    "revision": "8b9399342f680ee54ba9",
+    "url": "/maroom/static/css/main.e79e8837.chunk.css"
+  },
+  {
+    "revision": "1fb1b240949ac3bbb29f",
+    "url": "/maroom/static/js/2.29b011ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maroom/static/js/2.29b011ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9399342f680ee54ba9",
+    "url": "/maroom/static/js/main.c71405ee.chunk.js"
+  },
+  {
+    "revision": "d3ecf156466cc57c2277",
+    "url": "/maroom/static/js/runtime-main.f58780f8.js"
+  },
+  {
+    "revision": "fea966bae56decb43d5e6694a3e32f02",
+    "url": "/maroom/static/media/check.fea966ba.svg"
+  }
+]);
