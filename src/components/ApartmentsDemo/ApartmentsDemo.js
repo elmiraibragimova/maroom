b1285a94eafa0ea1455d0b482e19529457835434
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2020-03-22 18:08:42 +0300

    fix layout

--- src/components/ApartmentsDemo/ApartmentsDemo.js
+++ src/components/ApartmentsDemo/ApartmentsDemo.js
@@ -8,30 +8,32 @@ class ApartmentsDemo extends Component {
     return (
       <article className="ApartmentsDemo">
         <section className="ApartmentsDemo__intro">
-          <div className="ApartmentsDemo__info">
-            <h2>Ваша квартира в надежных руках</h2>
-            <p>
-              Мы оцениваем квартиру и даем рекомендации собственнику по
-              устранению недостатков в квартире, проверяем подлинность и
-              актуальность правоустанавливающих документов.
-            </p>
-          </div>
-          <div className="ApartmentsDemo__details">
-            <div>
-              <span className="ApartmentsDemo__accent">179</span>
-              <span className="ApartmentsDemo__description">
-                квартир в обслуживании
-              </span>
+          <div className="ApartmentsDemo__container">
+            <div className="ApartmentsDemo__info">
+              <h2>Ваша квартира в надежных руках</h2>
+              <p>
+                Мы оцениваем квартиру и даем рекомендации собственнику по
+                устранению недостатков в квартире, проверяем подлинность и
+                актуальность правоустанавливающих документов.
+              </p>
             </div>
-            <div>
-              <span className="ApartmentsDemo__accent">1482</span>
-              <span className="ApartmentsDemo__description">
-                часов сэкономлено
-              </span>
-            </div>
-            <div>
-              <span className="ApartmentsDemo__accent">5+</span>
-              <span className="ApartmentsDemo__description">лет работы</span>
+            <div className="ApartmentsDemo__details">
+              <div>
+                <span className="ApartmentsDemo__accent">179</span>
+                <span className="ApartmentsDemo__description">
+                  квартир в обслуживании
+                </span>
+              </div>
+              <div>
+                <span className="ApartmentsDemo__accent">1482</span>
+                <span className="ApartmentsDemo__description">
+                  часов сэкономлено
+                </span>
+              </div>
+              <div>
+                <span className="ApartmentsDemo__accent">5+</span>
+                <span className="ApartmentsDemo__description">лет работы</span>
+              </div>
             </div>
           </div>
         </section>
